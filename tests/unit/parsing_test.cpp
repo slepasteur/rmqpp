9c7e718488605ede5c99020aaf5d9438b1bcc51d
Simon Lepasteur <simon.lepasteur@swissdotnet.ch>
2020-05-20 18:16:35 +0200

    Use parse it in parsing test.

--- tests/unit/parsing_test.cpp
+++ tests/unit/parsing_test.cpp
@@ -2,6 +2,8 @@
 #include <cstddef>
 #include <span>
 
+#include <parse_it/parser.h>
+
 #include <doctest/doctest.h>
 
 constexpr std::byte operator"" _b(unsigned long long n)
@@ -11,5 +13,10 @@ constexpr std::byte operator"" _b(unsigned long long n)
 
 TEST_CASE("Parsing uint8_t (octet) type.")
 {
-  [[maybe_unused]] std::array buffer{0x12_b};
+  std::array buffer{0x12_b};
+  constexpr auto parser = parse_it::any_byte();
+  const auto parsing_result = parser(buffer);
+
+  REQUIRE(parsing_result);
+  REQUIRE(parsing_result.value().first == 0x12_b);
 }
