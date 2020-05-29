386090704eb6bbc004472433f1b8a882b1573f34
Simon Lepasteur <simon.lepasteur@swissdotnet.ch>
2020-05-29 18:20:15 +0200

    Add long string parser + parse ints as big endian.

--- src/transport/parsing/data_field_parser.h
+++ src/transport/parsing/data_field_parser.h
@@ -6,19 +6,29 @@
 
 namespace rmq {
 
-inline auto short_string_parser()
-{
-  return [](parse_it::parse_input_t input) -> parse_it::parse_result_t<std::string> {
-    constexpr auto size_parser = parse_it::uint8_parser();
-    auto size = size_parser(input);
+inline constexpr auto octet_parser = parse_it::integral_parser<std::uint8_t>();
+inline constexpr auto short_int_parser = parse_it::integral_parser<std::uint16_t>();
+inline constexpr auto long_int_parser = parse_it::integral_parser<std::uint32_t>();
+inline constexpr auto long_long_int_parser = parse_it::integral_parser<std::uint64_t>();
+inline constexpr auto short_string_parser = [](parse_it::parse_input_t input) -> parse_it::parse_result_t<std::string> {
+  auto size = octet_parser(input);
 
-    if (!size)
-    {
-      return std::nullopt;
-    }
-    auto content_parser = parse_it::n_bytes(size->first);
-    return parse_it::fmap(byte_span_to_string, content_parser)(size->second);
-  };
-}
+  if (!size)
+  {
+    return std::nullopt;
+  }
+  auto content_parser = parse_it::n_bytes(size->first);
+  return parse_it::fmap(byte_span_to_string, content_parser)(size->second);
+};
+inline constexpr auto long_string_parser = [](parse_it::parse_input_t input) -> parse_it::parse_result_t<std::string> {
+  auto size = long_int_parser(input);
+
+  if (!size)
+  {
+    return std::nullopt;
+  }
+  auto content_parser = parse_it::n_bytes(size->first);
+  return parse_it::fmap(byte_span_to_string, content_parser)(size->second);
+};
 
 } // namespace rmq
--- tests/unit/parsing_data_field_test.cpp
+++ tests/unit/parsing_data_field_test.cpp
@@ -21,8 +21,7 @@ constexpr std::byte operator"" _b(char c)
 TEST_CASE("Parsing uint8_t (octet) type.")
 {
   std::array buffer{0x12_b};
-  constexpr auto parser = parse_it::uint8_parser();
-  const auto parsing_result = parser(buffer);
+  const auto parsing_result = rmq::octet_parser(buffer);
 
   REQUIRE(parsing_result);
   REQUIRE(parsing_result.value().first == 0x12);
@@ -31,38 +30,43 @@ TEST_CASE("Parsing uint8_t (octet) type.")
 TEST_CASE("Parsing uint16_t (short integer) type.")
 {
   std::array buffer{0x12_b, 0x34_b};
-  constexpr auto parser = parse_it::uint16_parser();
-  const auto parsing_result = parser(buffer);
+  const auto parsing_result = rmq::short_int_parser(buffer);
 
   REQUIRE(parsing_result);
-  REQUIRE(parsing_result.value().first == 0x3412);
+  REQUIRE(parsing_result.value().first == 0x1234);
 }
 
 TEST_CASE("Parsing uint32_t (long integer) type.")
 {
   std::array buffer{0x12_b, 0x34_b, 0x56_b, 0x78_b};
-  constexpr auto parser = parse_it::uint32_parser();
-  const auto parsing_result = parser(buffer);
+  const auto parsing_result = rmq::long_int_parser(buffer);
 
   REQUIRE(parsing_result);
-  REQUIRE(parsing_result.value().first == 0x78563412);
+  REQUIRE(parsing_result.value().first == 0x12345678);
 }
 
 TEST_CASE("Parsing uint64_t (long long integer) type.")
 {
   std::array buffer{0x12_b, 0x34_b, 0x56_b, 0x78_b, 0x91_b, 0x23_b, 0x45_b, 0x67_b};
-  constexpr auto parser = parse_it::uint64_parser();
-  const auto parsing_result = parser(buffer);
+  const auto parsing_result = rmq::long_long_int_parser(buffer);
 
   REQUIRE(parsing_result);
-  REQUIRE(parsing_result.value().first == 0x6745239178563412ull);
+  REQUIRE(parsing_result.value().first == 0x1234567891234567ull);
 }
 
 TEST_CASE("Parsing a short string.")
 {
   std::array buffer{0x04_b, 'a'_b, 'b'_b, 'c'_b, 'd'_b};
-  auto parser = rmq::short_string_parser();
-  const auto parsing_result = parser(buffer);
+  const auto parsing_result = rmq::short_string_parser(buffer);
+
+  REQUIRE(parsing_result);
+  REQUIRE(parsing_result->first == std::string{"abcd"});
+}
+
+TEST_CASE("Parsing a long string.")
+{
+  std::array buffer{0x00_b, 0x00_b, 0x00_b, 0x04_b, 'a'_b, 'b'_b, 'c'_b, 'd'_b};
+  const auto parsing_result = rmq::long_string_parser(buffer);
 
   REQUIRE(parsing_result);
   REQUIRE(parsing_result->first == std::string{"abcd"});
